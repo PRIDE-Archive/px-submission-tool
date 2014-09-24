1930b54a1354b2f94ad3c46496b005993205f089
harry.r.wang@gmail.com <harry.r.wang@gmail.com@b7631ac0-c227-11dd-874b-8bacd04a0a74>
2014-09-24 08:19:40 +0000

    fixed a bug in using proxy

--- src/main/java/uk/ac/ebi/pride/gui/task/GetPrideUserDetailTask.java
+++ src/main/java/uk/ac/ebi/pride/gui/task/GetPrideUserDetailTask.java
@@ -1,10 +1,18 @@
 package uk.ac.ebi.pride.gui.task;
 
+import org.apache.http.HttpHost;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import uk.ac.ebi.pride.App;
 import uk.ac.ebi.pride.archive.submission.model.user.ContactDetail;
 import uk.ac.ebi.pride.gui.desktop.DesktopContext;
 import uk.ac.ebi.pride.web.util.template.SecureRestTemplateFactory;
+
+import java.util.Properties;
 
 /**
  * GetPrideUserDetailTask retrieves pride user details using pride web service
@@ -13,6 +21,8 @@ import uk.ac.ebi.pride.web.util.template.SecureRestTemplateFactory;
  * @version $Id$
  */
 public class GetPrideUserDetailTask extends TaskAdapter<ContactDetail, String> {
+
+    private static final Logger logger = LoggerFactory.getLogger(GetPrideUserDetailTask.class);
 
     private final RestTemplate restTemplate;
 
@@ -44,6 +54,19 @@ public class GetPrideUserDetailTask extends TaskAdapter<ContactDetail, String> {
         String baseUrl = context.getProperty("pride.user.detail.url");
 
         try {
+            // set proxy
+            Properties props = System.getProperties();
+            String proxyHost = props.getProperty("http.proxyHost");
+            String proxyPort = props.getProperty("http.proxyPort");
+
+            if (proxyHost != null && proxyPort != null) {
+                logger.info("Using proxy server {} and port {}", proxyHost, proxyPort);
+                HttpComponentsClientHttpRequestFactory factory = ((HttpComponentsClientHttpRequestFactory) restTemplate.getRequestFactory());
+                DefaultHttpClient defaultHttpClient = (DefaultHttpClient) factory.getHttpClient();
+                HttpHost proxy = new HttpHost(proxyHost.trim(), Integer.parseInt(proxyPort));
+                defaultHttpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+            }
+
             return restTemplate.getForObject(baseUrl, ContactDetail.class);
         } catch (Exception ex) {
             publish("Failed to login, please check user name or password");
