76dc4ab4f4fe8a1583019f0b992c97faea88d8a6
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-07 14:33:14 +0100

    TDD skeleton and resources

---
+++ src/test/java/uk/ac/ebi/pride/AppTest.java
@@ -0,0 +1,23 @@
+package uk.ac.ebi.pride;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride
+ * Timestamp: 2016-06-07 14:19
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+
+public class AppTest {
+    private final static Logger logger = LoggerFactory.getLogger(AppTest.class);
+
+    @Test
+    public void showUpTests() {
+        logger.debug("This test does nothing, really, just wiring testing subsystem");
+    }
+}
