f525121cd81d71eed97e9af269ef1d2264ce59ea
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-09 00:35:30 +0100

    mzTab Full document specialised parser, draft

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/MzTabFullDocumentParser.java
@@ -0,0 +1,27 @@
+package uk.ac.ebi.pride.gui.data.mztab.parser;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.parser
+ * Timestamp: 2016-06-08 23:52
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+public class MzTabFullDocumentParser extends MzTabParser {
+    private static final Logger logger = LoggerFactory.getLogger(MzTabFullDocumentParser.class);
+
+    // mzTab source
+    private String fileUrl;
+
+    public MzTabFullDocumentParser(String fileUrl) {
+        super();
+        this.fileUrl = fileUrl;
+        // TODO - Open file
+        // TODO - Set initial state
+    }
+
+}
