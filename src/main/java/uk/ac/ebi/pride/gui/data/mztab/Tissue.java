f85507a8331cf61466b22f5efca55bb9e1d3df43
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-08 14:12:32 +0100

    Tissue model included now

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/Tissue.java
@@ -0,0 +1,20 @@
+package uk.ac.ebi.pride.gui.data.mztab;
+
+import uk.ac.ebi.pride.gui.data.mztab.exceptions.InvalidCvParameterException;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab
+ * Timestamp: 2016-06-08 14:08
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ *
+ * This class models sample tissue information
+ */
+
+public class Tissue extends CvParameter {
+    public Tissue(String label, String accession, String name, String value) throws InvalidCvParameterException {
+        super(label, accession, name, value);
+    }
+}
