59a0f3214d8ccf91fae8318519bd59e43858c4d4
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-08 12:19:28 +0100

    ms-run format and id format

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/MsRunFormat.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.pride.gui.data.mztab;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab
+ * Timestamp: 2016-06-08 12:10
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+
+public class MsRunFormat extends CvParameter {
+    // Nothing special here
+}
---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/MsRunIdFormat.java
@@ -0,0 +1,17 @@
+package uk.ac.ebi.pride.gui.data.mztab;
+
+import com.itextpdf.text.log.Logger;
+import com.itextpdf.text.log.LoggerFactory;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab
+ * Timestamp: 2016-06-08 11:25
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+
+public class MsRunIdFormat extends CvParameter {
+    // Nothing special to do here
+}
