26211545691ec7e5d2acf9a3e5dffcee53a174d6
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-08 14:24:16 +0100

    Sample cell type, disease and custom attribute models

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/SampleCustomAttribute.java
@@ -0,0 +1,20 @@
+package uk.ac.ebi.pride.gui.data.mztab;
+
+import uk.ac.ebi.pride.gui.data.mztab.exceptions.InvalidCvParameterException;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab
+ * Timestamp: 2016-06-08 14:14
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ *
+ * This class models Sample custom parameters (sample[]-custom[])
+ */
+
+public class SampleCustomAttribute extends CvParameter {
+    public SampleCustomAttribute(String label, String accession, String name, String value) throws InvalidCvParameterException {
+        super(label, accession, name, value);
+    }
+}
