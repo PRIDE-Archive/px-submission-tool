92da001779d0c169d039086bc411df5a80f83a3f
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-08 12:36:59 +0100

    Sample model skeleton

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/Sample.java
@@ -0,0 +1,24 @@
+package uk.ac.ebi.pride.gui.data.mztab;
+
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab
+ * Timestamp: 2016-06-08 10:56
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ *
+ * This class models a Sample in mzTab metadata section
+ */
+
+public class Sample {
+    private static final Logger logger = LoggerFactory.getLogger(Sample.class);
+
+    // Bean
+    private String description;
+
+}
