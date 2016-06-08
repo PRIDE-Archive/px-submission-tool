3fc13b453f674e157069f83c0608e3ecd3668d27
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-09 00:34:56 +0100

    mzTab statefull parser abstract interface and state parser abstract interface

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/MzTabParser.java
@@ -0,0 +1,54 @@
+package uk.ac.ebi.pride.gui.data.mztab.parser;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.ac.ebi.pride.gui.data.mztab.model.MzTabDocument;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.parser
+ * Timestamp: 2016-06-08 21:25
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ *
+ * This class implements the mzTab file parser context, for a statefull parser of the different sections in the file
+ */
+public abstract class MzTabParser {
+    private static final Logger logger = LoggerFactory.getLogger(MzTabParser.class);
+
+    // Final Product
+    private MzTabDocument mzTabDocument;
+
+    // Parser State Delegate
+    private ParserState parserState;
+
+    protected MzTabParser() {
+        mzTabDocument = null;
+        parserState = null;
+    }
+
+    protected void setMzTabDocument(MzTabDocument mzTabDocument) {
+        this.mzTabDocument = mzTabDocument;
+    }
+
+    protected void setParserState(ParserState parserState) {
+        this.parserState = parserState;
+    }
+
+    // Return the product of this statefull builder
+    public MzTabDocument getMzTabDocument() {
+        return mzTabDocument;
+    }
+
+    // Director of the parsing process (build)
+    public final void parse() {
+        doParse();
+        doValidateProduct();
+    }
+
+    // Delegate steps
+    protected abstract void doParse();
+    protected abstract void doValidateProduct();
+
+}
---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/ParserState.java
@@ -0,0 +1,30 @@
+package uk.ac.ebi.pride.gui.data.mztab.parser;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.parser
+ * Timestamp: 2016-06-08 21:41
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+public abstract class ParserState {
+    private static final Logger logger = LoggerFactory.getLogger(ParserState.class);
+
+    // Parse a line
+    public abstract void parseLine(MzTabParser context, String line, long lineNumber, long offset);
+    // Change state delegate
+    protected final void changeState(MzTabParser context, ParserState newState) {
+        doValidateProduct(MzTabParser context);
+        doSetProduct(MzTabParser context);
+        doChangeState(MzTabParser context, ParserState newState);
+    }
+    // Delegate to subclasses
+    protected abstract void doValidateProduct(MzTabParser context);
+    protected abstract void doSetProduct(MzTabParser context);
+    protected abstract void doChangeState(MzTabParser context, ParserState newState);
+
+}
