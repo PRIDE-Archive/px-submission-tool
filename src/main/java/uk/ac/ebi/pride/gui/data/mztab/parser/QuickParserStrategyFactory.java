db33143d6fafbdcd9af48d67a3ad8a942dd91e5c
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-09 14:54:51 +0100

    Updated documentation

--- src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/QuickParserStrategyFactory.java
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/QuickParserStrategyFactory.java
@@ -10,6 +10,10 @@ import org.slf4j.LoggerFactory;
  * ---
  * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
  * All rights reserved.
+ *
+ * This Factory puts together a collection of ParserState objects for the different sections of mzTab files, that as a
+ * team, implement the "quick file parsing strategy", which is about collecting just the information we need from the
+ * mzTab file for the px-submission tool
  */
 
 public class QuickParserStrategyFactory implements StrategyParserStateFactory {
