52738fc31abe2e988250c6c9ee30140fa262a007
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-15 12:47:00 +0100

    Testing quick mzTab mode line item parser, work in progress

---
+++ src/test/java/uk/ac/ebi/pride/gui/data/mztab/parser/QuickMzTabModeLineItemParsingHandlerTest.java
@@ -0,0 +1,63 @@
+package uk.ac.ebi.pride.gui.data.mztab.parser;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import uk.ac.ebi.pride.gui.data.mztab.model.MetaData;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.parser
+ * Timestamp: 2016-06-15 11:47
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ *
+ * This is a collection of tests to run over a stand alone (last handler) QuickMzTabModeLineItemParsingHandler
+ */
+
+public class QuickMzTabModeLineItemParsingHandlerTest {
+    private static MzTabParser context;
+    private LineItemParsingHandler subject;
+
+    @BeforeClass
+    public static void initDummies() {
+        context = new DummyMzTabParser("dummyfile.txt");
+    }
+
+    @Before
+    public void init() {
+        subject = new QuickMzTabModeLineItemParsingHandler();
+    }
+
+    @Test
+    public void returnFalseOnEmptyLine() {
+        assertFalse("Return false when handling an empty line", subject.parseLine(context, "", 1, 0));
+    }
+
+    @Test
+    public void returnFalseWhenInvalidKeyword() {
+        assertFalse("Return false when invalid keyword", subject.parseLine(context, "MTD\tikjshdfksjhfd\tGarbage information", 1, 0));
+    }
+
+    @Test
+    public void processCompleteMode() {
+        MzTabParser context = Mockito.mock(DummyMzTabParser.class);
+        MetaData metaData = Mockito.mock(MetaData.class);
+        String mztabCompleteModeLine = "MTD\tmzTab-mode\tComplete";
+        when(context.getMetaDataSection()).thenReturn(metaData);
+        subject.parseLine(context, mztabCompleteModeLine, 1, 0);
+        verify(context, atLeastOnce()).getMetaDataSection();
+        verify(metaData, times(1)).getMode();
+        verify(metaData, times(1)).setMode(MetaData.MzTabMode.COMPLETE);
+    }
+}
