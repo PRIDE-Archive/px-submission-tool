5106d8ebfe8a1e21c25720ae89ba5bc7a866da9c
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-16 11:43:22 +0100

    copy and paste bug fixing ^_^

--- src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/MzTabVersionLineItemParsingHandler.java
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/MzTabVersionLineItemParsingHandler.java
@@ -23,11 +23,11 @@ public abstract class MzTabVersionLineItemParsingHandler extends MetaDataLineIte
         String[] lineItems = line.split("\t");
         if ((lineItems.length == 3) && (lineItems[1].equals(MZTAB_VERSION_KEYWORD))) {
             // No further checks are performed to mzTab version value
-            return doProcessMztabVersion(MzTabParser context, String line, long lineNumber, long offset, line[2]);
+            return doProcessMztabVersion(context, line, lineNumber, offset, lineItems[2]);
         }
         return false;
     }
 
     // Delegate processing
-    protected abstract boolean doProcessMztabVersion(MzTabParser context, String line, long lineNumber, long offset) throws LineItemParsingHandlerException;
+    protected abstract boolean doProcessMztabVersion(MzTabParser context, String line, long lineNumber, long offset, String versionValue) throws LineItemParsingHandlerException;
 }
