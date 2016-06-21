f1e0f860a25002697e290f641352358e26f2f06f
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-21 12:06:02 +0100

    ms-run ID format parser and quick processing strategy

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/QuickMzTabMsRunIdFormatLineItemParsingHandler.java
@@ -0,0 +1,29 @@
+package uk.ac.ebi.pride.gui.data.mztab.parser;
+
+import uk.ac.ebi.pride.gui.data.mztab.model.MsRunIdFormat;
+import uk.ac.ebi.pride.gui.data.mztab.parser.exceptions.LineItemParsingHandlerException;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.parser
+ * Timestamp: 2016-06-21 11:51
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+
+public class QuickMzTabMsRunIdFormatLineItemParsingHandler extends MzTabMsRunIdFormatLineItemParsingHandler {
+    private void checkForDuplicatedEntry(MzTabParser context, long lineNumber) throws LineItemParsingHandlerException {
+        if (context.getMetaDataSection().getMsRunEntry(getIndex()).getMsRunIdFormat() != null) {
+            throw new LineItemParsingHandlerException("DUPLICATED MS-Run ID Format FOUND AT LINE " + lineNumber);
+        }
+    }
+
+    @Override
+    protected boolean doProcessEntry(MzTabParser context, long lineNumber, long offset) throws LineItemParsingHandlerException {
+        checkForDuplicatedEntry(context, lineNumber);
+        // Process entry
+        context.getMetaDataSection().getMsRunEntry(getIndex()).setMsRunIdFormat(new MsRunIdFormat(CvParameterParser.fromString(getSubItemValueString())));
+        return true;
+    }
+}
