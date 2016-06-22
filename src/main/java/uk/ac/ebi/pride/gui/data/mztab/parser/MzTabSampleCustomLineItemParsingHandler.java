08d584fce55651d7246aefb277684fdc0f8938c3
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-22 16:56:56 +0100

    sample custom attribute check property key

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/MzTabSampleCustomLineItemParsingHandler.java
@@ -0,0 +1,26 @@
+package uk.ac.ebi.pride.gui.data.mztab.parser;
+
+import uk.ac.ebi.pride.gui.data.mztab.parser.exceptions.LineItemParsingHandlerException;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.parser
+ * Timestamp: 2016-06-22 16:55
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+public abstract class MzTabSampleCustomLineItemParsingHandler extends MzTabSampleLineItemParsingHandler {
+    protected static final String MZTAB_SAMPLE_CUSTOM_PROPERTY_KEY = "custom";
+
+    @Override
+    protected boolean processEntry(MzTabParser context, long lineNumber, long offset) throws LineItemParsingHandlerException {
+        if (getPropertyKey().equals(MZTAB_SAMPLE_CUSTOM_PROPERTY_KEY)) {
+            return doProcessEntry(context, lineNumber, offset);
+        }
+        return false;
+    }
+
+    // Delegate processing strategy
+    protected abstract boolean doProcessEntry(MzTabParser context, long lineNumber, long offset) throws LineItemParsingHandlerException;
+}
