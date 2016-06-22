195e3197e424bc601234b9b57ea0b627659f31dc
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-22 15:35:56 +0100

    sample line item parsing handler skeleton

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/MzTabSampleLineItemParsingHandler.java
@@ -0,0 +1,89 @@
+package uk.ac.ebi.pride.gui.data.mztab.parser;
+
+import uk.ac.ebi.pride.gui.data.mztab.model.Sample;
+import uk.ac.ebi.pride.gui.data.mztab.parser.exceptions.LineItemParsingHandlerException;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.parser
+ * Timestamp: 2016-06-22 13:26
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+public abstract class MzTabSampleLineItemParsingHandler extends MetaDataLineItemParsingHandler implements MetaDataLineItemParsingHandler.IndexedLineItemWithIndexedPropoertyDataEntry {
+
+    protected static final String MZTAB_SAMPLE_ITEM_PREFIX = "sample";
+    private String lineItemKey = "";
+    private int index = 0;
+    private String propertyKey = "";
+    private String propertyValue = "";
+    private int propertyEntryIndex = -1;
+
+    @Override
+    public String getLineItemKey() {
+        return lineItemKey;
+    }
+
+    @Override
+    public void setLineItemKey(String lineItemKey) {
+        this.lineItemKey = lineItemKey;
+    }
+
+    @Override
+    public int getIndex() {
+        return index;
+    }
+
+    @Override
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    @Override
+    public String getPropertyKey() {
+        return propertyKey;
+    }
+
+    @Override
+    public void setPropertyKey(String propertyKey) {
+        this.propertyKey = propertyKey;
+    }
+
+    @Override
+    public String getPropertyValue() {
+        return propertyValue;
+    }
+
+    @Override
+    public void setPropertyValue(String propertyValue) {
+        this.propertyValue = propertyValue;
+    }
+
+    @Override
+    public int getPropertyEntryIndex() {
+        return propertyEntryIndex;
+    }
+
+    @Override
+    public void setPropertyEntryIndex(int propertyEntryIndex) {
+        this.propertyEntryIndex = propertyEntryIndex;
+    }
+
+    protected Sample getSampleFromContext(MzTabParser context, int sampleIndex) {
+        Sample sample = context.getMetaDataSection().getSampleData(sampleIndex);
+        if (sample == null) {
+            sample = new Sample();
+            context.getMetaDataSection().updateSampleData(sample, sampleIndex);
+        }
+        return sample;
+    }
+
+    @Override
+    protected boolean doParseLineItem(MzTabParser context, String line, long lineNumber, long offset) throws LineItemParsingHandlerException {
+        return false;
+    }
+
+    // Delegate processing
+    protected abstract boolean processEntry(MzTabParser context, long lineNumber, long offset) throws LineItemParsingHandlerException;
+}
