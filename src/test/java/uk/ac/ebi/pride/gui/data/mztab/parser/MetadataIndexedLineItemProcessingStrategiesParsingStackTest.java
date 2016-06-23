939b44bc2323aad8f2871a324d759b7783131a68
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-23 23:30:53 +0100

    unit tests for indexed line item processing strategies parsing stacks

---
+++ src/test/java/uk/ac/ebi/pride/gui/data/mztab/parser/MetadataIndexedLineItemProcessingStrategiesParsingStackTest.java
@@ -0,0 +1,126 @@
+package uk.ac.ebi.pride.gui.data.mztab.parser;
+
+import net.jcip.annotations.NotThreadSafe;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import uk.ac.ebi.pride.gui.data.mztab.parser.exceptions.LineItemParsingHandlerException;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.parser
+ * Timestamp: 2016-06-23 15:19
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+@RunWith(Parameterized.class)
+@NotThreadSafe
+public class MetadataIndexedLineItemProcessingStrategiesParsingStackTest {
+    private String lineStart;
+    private String lineItemKey;
+    private int index;
+    private String propertyKey;
+    private String propertyValue;
+    private String testDescription;
+    private LineItemParsingHandler subject;
+    private DummyMzTabParser context;
+
+    private static String getMzTabLine(String section, String item, int itemIndex, String attribute, String attributeValue) {
+        return section + "\t" + item + "[" + itemIndex + "]-" + attribute + "\t" + attributeValue;
+    }
+
+    public MetadataIndexedLineItemProcessingStrategiesParsingStackTest(String lineStart,
+                                                                       String lineItemKey,
+                                                                       int index,
+                                                                       String propertyKey,
+                                                                       String propertyValue,
+                                                                       String testDescription,
+                                                                       LineItemParsingHandler subject) {
+        this.lineStart = lineStart;
+        this.lineItemKey = lineItemKey;
+        this.index = index;
+        this.propertyKey = propertyKey;
+        this.propertyValue = propertyValue;
+        this.testDescription = testDescription;
+        this.subject = subject;
+    }
+
+    @Before
+    public void prepareEmptyContext() {
+        this.context = new DummyMzTabParser("dummyfile");
+    }
+
+    // Test for manipulated line item key
+    @Test
+    public void rejectIncorrectLineItemKey() {
+        assertThat("mzTab line rejected because of line item key mismatch, by " + testDescription,
+                subject.parseLine(context, getMzTabLine(lineStart, "ñlkajsdflkj", index, propertyKey, propertyValue), 1, 0),
+                is(false));
+    }
+
+    // Test for manipulated line item property key
+    @Test
+    public void rejectIncorrectPropertyKey() {
+        assertThat("mzTab line rejected because of incorrect property key, by " + testDescription,
+                subject.parseLine(context, getMzTabLine(lineStart, lineItemKey, index, "lkasjdf", propertyValue), 1, 0),
+                is(false));
+    }
+
+    // Test for missing index
+    @Test(expected = LineItemParsingHandlerException.class)
+    public void rejectLineWithMissingIndex() {
+        String mzTabLine = getMzTabLine(lineStart, lineItemKey, index, propertyKey, propertyValue);
+        mzTabLine = mzTabLine.replaceFirst("\\[\\d+\\]", "[]");
+        subject.parseLine(context, mzTabLine, 1, 0);
+    }
+
+    // Test for bad index formatting
+    @Test(expected = LineItemParsingHandlerException.class)
+    public void rejectLineWithBadIndexFormatting() {
+        String mzTabLine = getMzTabLine(lineStart, lineItemKey, index, propertyKey, propertyValue);
+        mzTabLine = mzTabLine.replaceFirst("\\[\\d+\\]", "[34.98]");
+        subject.parseLine(context, mzTabLine, 1, 0);
+    }
+
+    // Test for NAN index
+    @Test(expected = LineItemParsingHandlerException.class)
+    public void rejectLineWithNanIndex() {
+        String mzTabLine = getMzTabLine(lineStart, lineItemKey, index, propertyKey, propertyValue);
+        mzTabLine = mzTabLine.replaceFirst("\\[\\d+\\]", "[Im_not_a_number]");
+        subject.parseLine(context, mzTabLine, 1, 0);
+    }
+
+    // Test for duplicated entry
+    @Test(expected = LineItemParsingHandlerException.class)
+    public void rejectDuplicatedEntry() {
+        String mzTabLine = getMzTabLine(lineStart, lineItemKey, index, propertyKey, propertyValue);
+        subject.parseLine(context, mzTabLine, 1, 0);
+        // Inserting the same line for the second time, triggers the error
+        subject.parseLine(context, mzTabLine, 1, 0);
+    }
+
+    // Everything ok test
+    @Test
+    public void successfulParsing() {
+        String mzTabLine = getMzTabLine(lineStart, lineItemKey, index, propertyKey, propertyValue);
+        assertThat("mzTab line parses successfuly for " + this.testDescription,
+                subject.parseLine(context, mzTabLine, 1, 0), is(true));
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> subjectsToTest() {
+        return Arrays.asList(new Object[][]{
+                {"MTD", "ms_run", 120, "format", "[PSI-MS, MS:1001062, Mascot MGF file, ]", "ms_run format", new QuickMzTabMsRunFormatLineItemParsingHandler()},
+                {"MTD", "ms_run", 120, "id_format", "[PSI-MS, MS:1000774, multiple peak list nativeID format, ]", "ms_run id_format", new QuickMzTabMsRunIdFormatLineItemParsingHandler()},
+                {"MTD", "ms_run", 120, "location", "file:Z:\\Proteomics\\CGehring\\ITurek\\20131123-1nM-Rep3\\Scaffold\\CM\\All3repSequest_Mascot mzIdent export 15-Sep-2014 04-39-39-AM\\TMT Sample 1\\Mudpit_20131006-IL-a24 (F027066).mzid_Mudpit_20131006-IL-a24_(F027066).MGF", "ms_run location", new QuickMzTabMsRunLocationLineItemParsingHandler()}
+        });
+    }
+}
