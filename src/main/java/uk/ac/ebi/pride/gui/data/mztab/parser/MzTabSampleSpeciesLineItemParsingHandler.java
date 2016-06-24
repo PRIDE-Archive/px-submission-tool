d37d9238cff6535b8bce0e601ebd82e23e1ef061
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-24 15:51:02 +0100

    sample species is an property in an index data entry, check for index presence

--- src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/MzTabSampleSpeciesLineItemParsingHandler.java
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/MzTabSampleSpeciesLineItemParsingHandler.java
@@ -16,6 +16,11 @@ public abstract class MzTabSampleSpeciesLineItemParsingHandler extends MzTabSamp
     @Override
     protected boolean processEntry(MzTabParser context, long lineNumber, long offset) throws LineItemParsingHandlerException {
         if (getPropertyKey().equals(MZTAB_SAMPLE_SPECIES_PROPERTY_KEY)) {
+            // Check that we have indexes for species property key
+            if (getPropertyEntryIndex() == DEFAULT_PROPERTY_ENTRY_INDEX) {
+                throw new LineItemParsingHandlerException("Missing index for property key '" + MZTAB_SAMPLE_SPECIES_PROPERTY_KEY + "'");
+            }
+
             return doProcessEntry(context, lineNumber, offset);
         }
         return false;
