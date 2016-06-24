f19e28fb2742d83381ee1c41befd57d21c02689e
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-24 16:36:19 +0100

    sample disease parser check for property index

--- src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/MzTabSampleDiseaseLineItemParsingHandler.java
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/MzTabSampleDiseaseLineItemParsingHandler.java
@@ -16,6 +16,9 @@ public abstract class MzTabSampleDiseaseLineItemParsingHandler extends MzTabSamp
     @Override
     protected boolean processEntry(MzTabParser context, long lineNumber, long offset) throws LineItemParsingHandlerException {
         if (getPropertyKey().equals(MZTAB_SAMPLE_DISEASE_PROPERTY_KEY)) {
+            if (getPropertyEntryIndex() == DEFAULT_PROPERTY_ENTRY_INDEX) {
+                throw new LineItemParsingHandlerException("Missing index for property key '" + MZTAB_SAMPLE_DISEASE_PROPERTY_KEY + "'");
+            }
             return doProcessEntry(context, lineNumber, offset);
         }
         return false;
