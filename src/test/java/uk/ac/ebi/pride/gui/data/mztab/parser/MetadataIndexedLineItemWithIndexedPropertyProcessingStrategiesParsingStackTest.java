f5564045534d5180671943065fc02f7ba76b83ce
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-24 12:35:09 +0100

    Preparing more tests

--- src/test/java/uk/ac/ebi/pride/gui/data/mztab/parser/MetadataIndexedLineItemWithIndexedPropertyProcessingStrategiesParsingStackTest.java
+++ src/test/java/uk/ac/ebi/pride/gui/data/mztab/parser/MetadataIndexedLineItemWithIndexedPropertyProcessingStrategiesParsingStackTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import uk.ac.ebi.pride.gui.data.mztab.parser.exceptions.LineItemParsingHandlerException;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -74,6 +75,47 @@ public class MetadataIndexedLineItemWithIndexedPropertyProcessingStrategiesParsi
                 subject.parseLine(context, getMzTabLine(lineStart, "ñlkajsdflkj", index, propertyKey, propertyEntryIndex, propertyValue), 1, 0),
                 is(false));
     }
+
+    // Test for manipulated line item property key
+    /*@Test
+    public void rejectIncorrectPropertyKey() {
+        assertThat("mzTab line rejected because of incorrect property key, by " + testDescription,
+                subject.parseLine(context, getMzTabLine(lineStart, lineItemKey, index, "lkasjdf", propertyValue), 1, 0),
+                is(false));
+    }
+
+    // Test for missing index
+    @Test(expected = LineItemParsingHandlerException.class)
+    public void rejectLineWithMissingIndex() {
+        String mzTabLine = getMzTabLine(lineStart, lineItemKey, index, propertyKey, propertyValue);
+        mzTabLine = mzTabLine.replaceFirst("\\[\\d+\\]", "[]");
+        subject.parseLine(context, mzTabLine, 1, 0);
+    }
+
+    // Test for bad index formatting
+    @Test(expected = LineItemParsingHandlerException.class)
+    public void rejectLineWithBadIndexFormatting() {
+        String mzTabLine = getMzTabLine(lineStart, lineItemKey, index, propertyKey, propertyValue);
+        mzTabLine = mzTabLine.replaceFirst("\\[\\d+\\]", "[34.98]");
+        subject.parseLine(context, mzTabLine, 1, 0);
+    }
+
+    // Test for NAN index
+    @Test(expected = LineItemParsingHandlerException.class)
+    public void rejectLineWithNanIndex() {
+        String mzTabLine = getMzTabLine(lineStart, lineItemKey, index, propertyKey, propertyValue);
+        mzTabLine = mzTabLine.replaceFirst("\\[\\d+\\]", "[Im_not_a_number]");
+        subject.parseLine(context, mzTabLine, 1, 0);
+    }
+
+    // Test for duplicated entry
+    @Test(expected = LineItemParsingHandlerException.class)
+    public void rejectDuplicatedEntry() {
+        String mzTabLine = getMzTabLine(lineStart, lineItemKey, index, propertyKey, propertyValue);
+        subject.parseLine(context, mzTabLine, 1, 0);
+        // Inserting the same line for the second time, triggers the error
+        subject.parseLine(context, mzTabLine, 1, 0);
+    }*/
 
     // Everything ok test
     @Test
