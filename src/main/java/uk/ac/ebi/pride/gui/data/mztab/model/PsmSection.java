94079082318dc91415d367799e3df61d55f5a1bb
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-27 16:23:23 +0100

    PSM section data manager

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/model/PsmSection.java
@@ -0,0 +1,79 @@
+package uk.ac.ebi.pride.gui.data.mztab.model;
+
+import uk.ac.ebi.pride.gui.data.mztab.exceptions.InvalidProteinSection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.model
+ * Timestamp: 2016-06-27 16:17
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ *
+ * This class manages PSM data in an mzTab document
+ */
+
+public class PsmSection {
+
+    public enum ColumnType {
+        SEQUENCE,
+        PSM_ID,
+        ACCESSION,
+        UNIQUE,
+        DATABASE,
+        DATABASE_VERSION,
+        SEARCH_ENGINE,
+        SEARCH_ENGINE_SCORE,
+        MODIFICATIONS,
+        SPECTRA_REF,
+        RETENTION_TIME,
+        CHARGE,
+        EXP_MASS_TO_CHARGE,
+        CALC_MASS_TO_CHARGE,
+        PRE,
+        POST,
+        START,
+        END,
+        OPT_CUSTOM_ATTRIBUTE,
+        RELIABILITY,
+        URI
+    }
+
+    // Present columns
+    private Set<PsmSection.ColumnType> columnsFound = new HashSet<>();
+
+    /**
+     * Report a column present at the given index
+     *
+     * @param index      where the reported column type has been found
+     * @param columnType column type found
+     */
+    public void addColumn(int index, PsmSection.ColumnType columnType) {
+        // NOTE - We ignore the index, as we don't need it at this stage
+        columnsFound.add(columnType);
+    }
+
+    /**
+     * Returns true if a particular column type has been reported, at least once
+     * @param columnType to look for
+     * @return true if a particular column type has been reported, false otherwise
+     */
+    public boolean isColumnTypePresent(PsmSection.ColumnType columnType) {
+        return columnsFound.contains(columnType);
+    }
+
+    /**
+     * Validation algorithm for this section is, in part, coupled with the metadata section that describes the
+     * mzTab document it belongs to.
+     *
+     * @param metaData Metadata section that describes the mzTab document where this section belongs to
+     * @throws InvalidProteinSection thrown if this protein section is not valid
+     */
+    public void validate(MetaData metaData) throws InvalidProteinSection {
+        // TODO
+    }
+
+}
