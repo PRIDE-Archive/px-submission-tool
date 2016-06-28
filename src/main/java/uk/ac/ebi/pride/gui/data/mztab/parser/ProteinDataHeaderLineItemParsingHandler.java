76ec018bce61c8b09818810df3fd62c42f5c35b9
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-28 16:12:54 +0100

    column mapper first version

--- src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/ProteinDataHeaderLineItemParsingHandler.java
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/ProteinDataHeaderLineItemParsingHandler.java
@@ -19,6 +19,72 @@ public abstract class ProteinDataHeaderLineItemParsingHandler extends ProteinDat
             if (token.matches("^accession$")) {
                 return ProteinData.ColumnType.ACCESSION;
             }
+            if (token.matches("^description$")) {
+                return ProteinData.ColumnType.DESCRIPTION;
+            }
+            if (token.matches("^taxid$")) {
+                return ProteinData.ColumnType.TAXID;
+            }
+            if (token.matches("^species$")) {
+                return ProteinData.ColumnType.SPECIES;
+            }
+            if (token.matches("^database$")) {
+                return ProteinData.ColumnType.DATABASE;
+            }
+            if (token.matches("^database_version$")) {
+                return ProteinData.ColumnType.DATABASE_VERSION;
+            }
+            if (token.matches("^search_engine$")) {
+                return ProteinData.ColumnType.SEARCH_ENGINE;
+            }
+            if (token.matches("^best_search_engine_score\\[\\d+\\]$")) {
+                return ProteinData.ColumnType.BEST_SEARCH_ENGINE_SCORE;
+            }
+            if (token.matches("^ambiguity_members$")) {
+                return ProteinData.ColumnType.AMBIGUITY_MEMBERS;
+            }
+            if (token.matches("^modifications$")) {
+                return ProteinData.ColumnType.MODIFICATIONS;
+            }
+            if (token.matches("^protein_coverage$")) {
+                return ProteinData.ColumnType.PROTEIN_COVERAGE;
+            }
+            if (token.matches("^protein_abundance_study_variable\\[\\d+\\]$")) {
+                return ProteinData.ColumnType.PROTEIN_ABUNDANCE_STUDY_VARIABLE;
+            }
+            if (token.matches("^protein_abundance_stdev_study_variable\\[\\d+\\]$")) {
+                return ProteinData.ColumnType.PROTEIN_ABUNDANCE_STDEV_STUDY_VARIABLE;
+            }
+            if (token.matches("^protein_abundance_std_error_study_variable\\[\\d+\\]$")) {
+                return ProteinData.ColumnType.PROTEIN_ABUNDANCE_STD_ERROR_STUDY_VARIABLE;
+            }
+            if (token.matches("^search_engine_score\\[\\d+\\]_ms_run\\[\\d+\\]$")) {
+                return ProteinData.ColumnType.SEARCH_ENGINE_SCORE_MS_RUN;
+            }
+            if (token.matches("^num_psms_ms_run\\[\\d+\\]$")) {
+                return ProteinData.ColumnType.NUM_PSMS_MS_RUN;
+            }
+            if (token.matches("^num_peptides_distinct_ms_run\\[\\d+\\]$")) {
+                return ProteinData.ColumnType.NUM_PEPTIDES_DISTINCT_MS_RUN;
+            }
+            if (token.matches("^num_peptide_unique_ms_run\\[\\d+\\]$")) {
+                return ProteinData.ColumnType.NUM_PEPTIDE_UNIQUE_MS_RUN;
+            }
+            if (token.matches("^protein_abundance_assay\\[\\d+\\]$")) {
+                return ProteinData.ColumnType.PROTEIN_ABUNDANCE_ASSAY;
+            }
+            if (token.startsWith("opt_")) {
+                return ProteinData.ColumnType.OPT_CUSTOM_ATTIBUTE;
+            }
+            if (token.matches("^go_terms$")) {
+                return ProteinData.ColumnType.GO_TERMS;
+            }
+            if (token.matches("^reliability$")) {
+                return ProteinData.ColumnType.RELIABILITY;
+            }
+            if (token.matches("^uri$")) {
+                return ProteinData.ColumnType.URI;
+            }
             return null;
         }
     }
