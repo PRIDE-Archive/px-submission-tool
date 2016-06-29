267476ce9812c3095341bf98869f65ccff279b81
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-29 15:04:23 +0100

    peptide parsing stack, data entry level

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/PeptideDataEntryLineItemParsingHandler.java
@@ -0,0 +1,33 @@
+package uk.ac.ebi.pride.gui.data.mztab.parser;
+
+import uk.ac.ebi.pride.gui.data.mztab.parser.exceptions.LineItemParsingHandlerException;
+
+import java.util.Arrays;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.parser
+ * Timestamp: 2016-06-29 14:56
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+public abstract class PeptideDataEntryLineItemParsingHandler extends PeptideDataLineItemParsingHandler {
+    private static final String MZTAB_PEPTIDE_DATA_ENTRY_LINE_KEYWORD = "PEP";
+
+    @Override
+    protected boolean doParseLineItem(MzTabParser context, String line, long lineNumber, long offset) throws LineItemParsingHandlerException {
+        String[] lineItems = line.split("\t");
+        if (lineItems[0].equals(MZTAB_PEPTIDE_DATA_ENTRY_LINE_KEYWORD)) {
+            try {
+                return doProcessDataEntryLine(context, Arrays.copyOfRange(lineItems, 1, lineItems.length), lineNumber, offset);
+            } catch (Exception e) {
+                throw new LineItemParsingHandlerException(e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    // Delegate processing
+    protected abstract boolean doProcessDataEntryLine(MzTabParser context, String[] parsedColumnEntries, long lineNumber, long offset) throws LineItemParsingHandlerException;
+}
