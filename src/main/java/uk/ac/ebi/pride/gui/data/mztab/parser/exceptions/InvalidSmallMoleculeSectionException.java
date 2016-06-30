1a3330e3fba1f1e2f7685306c0c5dcf2b59e13d1
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-06-30 13:02:29 +0100

    Missing invalid section exceptions

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/exceptions/InvalidSmallMoleculeSectionException.java
@@ -0,0 +1,15 @@
+package uk.ac.ebi.pride.gui.data.mztab.parser.exceptions;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.parser.exceptions
+ * Timestamp: 2016-06-30 13:00
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+public class InvalidSmallMoleculeSectionException extends RuntimeException {
+    public InvalidSmallMoleculeSectionException(String message) {
+        super(message);
+    }
+}
