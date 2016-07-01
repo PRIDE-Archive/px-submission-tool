08f663bd04bbce7ec22709202cd3d70c8add070a
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-07-01 14:18:26 +0100

    weird changes pointed out by git

--- src/main/java/uk/ac/ebi/pride/gui/data/mztab/model/OneTimeDefaultValidatorMzTabSectionValidator.java
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/model/OneTimeDefaultValidatorMzTabSectionValidator.java
@@ -7,33 +7,55 @@ package uk.ac.ebi.pride.gui.data.mztab.model;
  * ---
  * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
  * All rights reserved.
+ *
+ * This subclass makes sure that the default validation strategy is run only once per mzTab section
  */
 public class OneTimeDefaultValidatorMzTabSectionValidator extends DefaultMzTabSectionValidator {
     private boolean hasBeenValidated = false;
     private boolean isValid = false;
 
     @Override
-    public boolean validate(MzTabDocument mzTabDocument, MetaData metaData) {
-        return super.validate(mzTabDocument, metaData);
+    public boolean validate(MzTabDocument mzTabDocument, MetaData metaData) throws MzTabSectionValidatorException {
+        if (!hasBeenValidated) {
+            isValid = super.validate(mzTabDocument, metaData);
+            hasBeenValidated = true;
+        }
+        return isValid;
     }
 
     @Override
-    public boolean validate(MzTabDocument mzTabDocument, ProteinData proteinData) {
-        return super.validate(mzTabDocument, proteinData);
+    public boolean validate(MzTabDocument mzTabDocument, ProteinData proteinData) throws MzTabSectionValidatorException {
+        if (!hasBeenValidated) {
+            isValid = super.validate(mzTabDocument, proteinData);
+            hasBeenValidated = true;
+        }
+        return isValid;
     }
 
     @Override
-    public boolean validate(MzTabDocument mzTabDocument, PeptideData peptideData) {
-        return super.validate(mzTabDocument, peptideData);
+    public boolean validate(MzTabDocument mzTabDocument, PeptideData peptideData) throws MzTabSectionValidatorException {
+        if (!hasBeenValidated) {
+            isValid = super.validate(mzTabDocument, peptideData);
+            hasBeenValidated = true;
+        }
+        return isValid;
     }
 
     @Override
-    public boolean validate(MzTabDocument mzTabDocument, PsmData psmData) {
-        return super.validate(mzTabDocument, psmData);
+    public boolean validate(MzTabDocument mzTabDocument, PsmData psmData) throws MzTabSectionValidatorException {
+        if (!hasBeenValidated) {
+            isValid = super.validate(mzTabDocument, psmData);
+            hasBeenValidated = true;
+        }
+        return isValid;
     }
 
     @Override
-    public boolean validate(MzTabDocument mzTabDocument, SmallMoleculeData smallMoleculeData) {
-        return super.validate(mzTabDocument, smallMoleculeData);
+    public boolean validate(MzTabDocument mzTabDocument, SmallMoleculeData smallMoleculeData) throws MzTabSectionValidatorException {
+        if (!hasBeenValidated) {
+            isValid = super.validate(mzTabDocument, smallMoleculeData);
+            hasBeenValidated = true;
+        }
+        return isValid;
     }
 }
