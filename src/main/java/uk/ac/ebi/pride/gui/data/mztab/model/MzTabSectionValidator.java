351acefbadb72be877ba20960f75262e43958538
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-07-01 13:52:12 +0100

    Section validation strategy interface

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/model/MzTabSectionValidator.java
@@ -0,0 +1,15 @@
+package uk.ac.ebi.pride.gui.data.mztab.model;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.model
+ * Timestamp: 2016-07-01 13:33
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ *
+ * This is kind of a visitor that externalized
+ */
+public class MzTabSectionValidator {
+
+}
