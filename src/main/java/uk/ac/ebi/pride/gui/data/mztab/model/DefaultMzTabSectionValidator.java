790cea70be830a3a1171de196ae82dda6d61b864
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-07-01 16:54:19 +0100

    metadata section validation strategy, first draft

--- src/main/java/uk/ac/ebi/pride/gui/data/mztab/model/DefaultMzTabSectionValidator.java
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/model/DefaultMzTabSectionValidator.java
@@ -1,5 +1,8 @@
 package uk.ac.ebi.pride.gui.data.mztab.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Project: px-submission-tool
  * Package: uk.ac.ebi.pride.gui.data.mztab.model
@@ -9,10 +12,69 @@ package uk.ac.ebi.pride.gui.data.mztab.model;
  * All rights reserved.
  */
 public class DefaultMzTabSectionValidator extends MzTabSectionValidator {
+    private static final Logger logger = LoggerFactory.getLogger(DefaultMzTabSectionValidator.class);
+
+    /**
+     * This is part of the validation strategy for mzTab metadata section, as we care only about what we require to be
+     * present in terms of this section alone.
+     * @param mzTabDocument
+     * @param metaData
+     * @return
+     * @throws MzTabSectionValidatorException
+     */
     @Override
     public boolean validate(MzTabDocument mzTabDocument, MetaData metaData) throws MzTabSectionValidatorException {
-        // TODO
-        return false;
+        // Check we have a title
+        if (metaData.getTitle() == null) {
+            logger.error("Missing title in mzTab metadata section!");
+            return false;
+        }
+        if (metaData.getVersion() == null) {
+            logger.error("Missing version information in mzTab metadata section!");
+            return false;
+        }
+        if (metaData.getType() == null) {
+            logger.error("Missing 'type' information in mzTab metadata section!");
+            return false;
+        }
+        if (metaData.getFileId() == null) {
+            logger.error("Missin 'file ID' information in mzTab metadata section!");
+            return false;
+        }
+        if (metaData.getMode() == null) {
+            logger.error("NO mzTab MODE has been specified!");
+            return false;
+        }
+        // Validate ms-run entries
+        for (int msRunIndex :
+                metaData.getAvailableMsRunIndexes()) {
+            try {
+                if (!metaData.getMsRunEntry(msRunIndex).validate()) {
+                    logger.error("INVALID metadata ms-run entry with index '" + msRunIndex + "'");
+                    return false;
+                }
+            } catch (ValidationException e) {
+                throw new MzTabSectionValidatorException("ERROR while validating ms-run metadata entry with index '"
+                        + msRunIndex + "' due to " + e.getMessage());
+            }
+        }
+        if (metaData.getDescription() == null) {
+            logger.warn("No 'description' information for the parsed mzTab document");
+        }
+        for (int sampleIndex :
+                metaData.getAvailableSampleIndexes()) {
+            try {
+                if (!metaData.getSampleData(sampleIndex).validate()) {
+                    logger.error("INVALID metadata sample entry with index '" + sampleIndex + "'");
+                    return false;
+                }
+            } catch (ValidationException e) {
+                throw new MzTabSectionValidatorException("ERROR while validating sample metadata entry with index '"
+                        + sampleIndex + "' due to " + e.getMessage());
+            }
+        }
+        // Up to this point, this section validates
+        return true;
     }
 
     @Override
