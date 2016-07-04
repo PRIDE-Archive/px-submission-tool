b2cdd868af404df04c74a5afc99054f42f73d707
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-07-04 16:24:34 +0100

    metadata and protein validation strategy draft

--- src/main/java/uk/ac/ebi/pride/gui/data/mztab/model/DefaultMzTabSectionValidator.java
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/model/DefaultMzTabSectionValidator.java
@@ -3,6 +3,10 @@ package uk.ac.ebi.pride.gui.data.mztab.model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Project: px-submission-tool
  * Package: uk.ac.ebi.pride.gui.data.mztab.model
@@ -29,21 +33,31 @@ public class DefaultMzTabSectionValidator extends MzTabSectionValidator {
             logger.error("Missing title in mzTab metadata section!");
             return false;
         }
+        // From the mzTab specification
+        // mzTab version MUST always be reported
         if (metaData.getVersion() == null) {
-            logger.error("Missing version information in mzTab metadata section!");
+            logger.error("MISSING mzTab Version information");
             return false;
         }
-        if (metaData.getType() == null) {
-            logger.error("Missing 'type' information in mzTab metadata section!");
-            return false;
-        }
-        if (metaData.getFileId() == null) {
-            logger.error("Missin 'file ID' information in mzTab metadata section!");
-            return false;
-        }
+        // mzTab mode MUST always be reported
         if (metaData.getMode() == null) {
-            logger.error("NO mzTab MODE has been specified!");
+            logger.error("MISSING mzTab Mode information");
             return false;
+        }
+        // mzTab type MUST always be reported
+        if (metaData.getType() == null) {
+            logger.error("MISSING mzTab Type information");
+            return false;
+        }
+        // description MUST always be reported
+        if (metaData.getDescription() == null) {
+            logger.error("MISSING mzTab Description information");
+            return false;
+        }
+        // Check for ID_file
+        if (metaData.getFileId() == null) {
+            // TODO - Revisit this in the future to make sure file ID is not mandatory
+            logger.warn("Missin 'file ID' information in mzTab metadata section!");
         }
         // Validate ms-run entries
         for (int msRunIndex :
@@ -58,8 +72,10 @@ public class DefaultMzTabSectionValidator extends MzTabSectionValidator {
                         + msRunIndex + "' due to " + e.getMessage());
             }
         }
-        if (metaData.getDescription() == null) {
-            logger.warn("No 'description' information for the parsed mzTab document");
+        // ms-run location MUST always be reported, for this section, it means that there is, at least, one ms-run
+        if (metaData.getAvailableMsRunIndexes().size() == 0) {
+            logger.error("ms-run location MUST be reported, but it is MISSING in the current mzTab data");
+            return false;
         }
         for (int sampleIndex :
                 metaData.getAvailableSampleIndexes()) {
@@ -73,14 +89,100 @@ public class DefaultMzTabSectionValidator extends MzTabSectionValidator {
                         + sampleIndex + "' due to " + e.getMessage());
             }
         }
+        // TODO Cross-check *_search_engine_score with their corresponding sections
+        // TODO fixed_mod and variable_mod MUST be reported
         // Up to this point, this section validates
         return true;
     }
 
     @Override
     public boolean validate(MzTabDocument mzTabDocument, ProteinData proteinData) throws MzTabSectionValidatorException {
-        // TODO
-       return false;
+        // Required columns for mzTab Identification type in Summary mode
+        Set<ProteinData.ColumnType> id_sum = new HashSet<>();
+        // Required columns for mzTab Identification type in Complete mode
+        Set<ProteinData.ColumnType> id_com = new HashSet<>();
+        // Required columns for mzTab Quantification type in Summary mode
+        Set<ProteinData.ColumnType> qu_sum = new HashSet<>();
+        // Required columns for mzTab Quantification type in Complete mode
+        Set<ProteinData.ColumnType> qu_com = new HashSet<>();
+        // Supported modes
+        Set<MetaData.MzTabMode> supportedModes = new HashSet<>(Arrays.asList(MetaData.MzTabMode.COMPLETE, MetaData.MzTabMode.SUMMARY));
+        // Supported types
+        Set<MetaData.MzTabType> supportedTypes = new HashSet<>(Arrays.asList(MetaData.MzTabType.IDENTIFICATION, MetaData.MzTabType.QUANTIFICATION));
+        // Fill in requirements sets
+        id_sum.addAll(Arrays.asList(
+                ProteinData.ColumnType.ACCESSION, ProteinData.ColumnType.DESCRIPTION, ProteinData.ColumnType.TAXID,
+                ProteinData.ColumnType.SPECIES, ProteinData.ColumnType.DATABASE, ProteinData.ColumnType.DATABASE_VERSION,
+                ProteinData.ColumnType.SEARCH_ENGINE, ProteinData.ColumnType.BEST_SEARCH_ENGINE_SCORE,
+                ProteinData.ColumnType.AMBIGUITY_MEMBERS, ProteinData.ColumnType.MODIFICATIONS
+        ));
+        id_com.addAll(Arrays.asList(
+                ProteinData.ColumnType.ACCESSION, ProteinData.ColumnType.DESCRIPTION, ProteinData.ColumnType.TAXID,
+                ProteinData.ColumnType.SPECIES, ProteinData.ColumnType.DATABASE, ProteinData.ColumnType.DATABASE_VERSION,
+                ProteinData.ColumnType.SEARCH_ENGINE, ProteinData.ColumnType.BEST_SEARCH_ENGINE_SCORE,
+                ProteinData.ColumnType.AMBIGUITY_MEMBERS, ProteinData.ColumnType.MODIFICATIONS,
+                ProteinData.ColumnType.PROTEIN_COVERAGE, ProteinData.ColumnType.SEARCH_ENGINE_SCORE_MS_RUN,
+                ProteinData.ColumnType.NUM_PSMS_MS_RUN, ProteinData.ColumnType.NUM_PEPTIDES_DISTINCT_MS_RUN,
+                ProteinData.ColumnType.NUM_PEPTIDES_UNIQUE_MS_RUN
+        ));
+        qu_sum.addAll(Arrays.asList(
+                ProteinData.ColumnType.ACCESSION, ProteinData.ColumnType.DESCRIPTION, ProteinData.ColumnType.TAXID,
+                ProteinData.ColumnType.SPECIES, ProteinData.ColumnType.DATABASE, ProteinData.ColumnType.DATABASE_VERSION,
+                ProteinData.ColumnType.SEARCH_ENGINE, ProteinData.ColumnType.BEST_SEARCH_ENGINE_SCORE,
+                ProteinData.ColumnType.AMBIGUITY_MEMBERS, ProteinData.ColumnType.MODIFICATIONS,
+                ProteinData.ColumnType.PROTEIN_ABUNDANCE_STUDY_VARIABLE, ProteinData.ColumnType.PROTEIN_ABUNDANCE_STDEV_STUDY_VARIABLE,
+                ProteinData.ColumnType.PROTEIN_ABUNDANCE_STD_ERROR_STUDY_VARIABLE
+        ));
+        qu_com.addAll(Arrays.asList(
+                ProteinData.ColumnType.ACCESSION, ProteinData.ColumnType.DESCRIPTION, ProteinData.ColumnType.TAXID,
+                ProteinData.ColumnType.SPECIES, ProteinData.ColumnType.DATABASE, ProteinData.ColumnType.DATABASE_VERSION,
+                ProteinData.ColumnType.SEARCH_ENGINE, ProteinData.ColumnType.BEST_SEARCH_ENGINE_SCORE,
+                ProteinData.ColumnType.AMBIGUITY_MEMBERS, ProteinData.ColumnType.MODIFICATIONS,
+                ProteinData.ColumnType.PROTEIN_COVERAGE, ProteinData.ColumnType.PROTEIN_ABUNDANCE_STUDY_VARIABLE,
+                ProteinData.ColumnType.PROTEIN_ABUNDANCE_STDEV_STUDY_VARIABLE,
+                ProteinData.ColumnType.PROTEIN_ABUNDANCE_STD_ERROR_STUDY_VARIABLE,
+                ProteinData.ColumnType.SEARCH_ENGINE_SCORE_MS_RUN, ProteinData.ColumnType.PROTEIN_ABUNDANCE_ASSAY
+        ));
+        // Check for supported mode
+        if (!supportedModes.contains(mzTabDocument.getMetaData().getMode())) {
+            throw new MzTabSectionValidatorException("VALIDATION OF MZTAB MODE '" + mzTabDocument.getMetaData().getMode() + "' IS NOT SUPPORTED");
+        }
+        // Check for supported type
+        if (!supportedTypes.contains(mzTabDocument.getMetaData().getType())) {
+            throw new MzTabSectionValidatorException("VALIDATION OF MZTAB TYPE '" + mzTabDocument.getMetaData().getType() + "' IS NOT SUPPORTED");
+        }
+        // Select the testing set
+        Set<ProteinData.ColumnType> testingSet = null;
+        if (mzTabDocument.getMetaData().getType() == MetaData.MzTabType.IDENTIFICATION) {
+            if (mzTabDocument.getMetaData().getMode() == MetaData.MzTabMode.SUMMARY) {
+                // identification type - summary mode
+                testingSet = id_sum;
+            } else if (mzTabDocument.getMetaData().getMode() == MetaData.MzTabMode.COMPLETE) {
+                // identification type - complete mode
+                testingSet = id_com;
+            }
+        } else if (mzTabDocument.getMetaData().getType() == MetaData.MzTabType.QUANTIFICATION) {
+            if (mzTabDocument.getMetaData().getMode() == MetaData.MzTabMode.SUMMARY) {
+                // quantification type - summary mode
+                testingSet = qu_sum;
+            } else if (mzTabDocument.getMetaData().getMode() == MetaData.MzTabMode.COMPLETE) {
+                // quantification type - complete mode
+                testingSet = qu_com;
+            }
+        }
+        // Check the required columns
+        if (!proteinData.checkThatAllGivenColumnTypesArePresent(testingSet)) {
+            logger.error("Protein section IS NOT VALID because of the following missing required columns:");
+            for (ProteinData.ColumnType missingColumnType :
+                    proteinData.getMissingColumnTypesFromRequiredColumnTypes(testingSet)) {
+                logger.error("MISSING Protein Data column '" + missingColumnType.toString() + "' for mzTab "
+                        + mzTabDocument.getMetaData().getType().toString() + " Type, "
+                        + mzTabDocument.getMetaData().getMode().toString() + " Mode");
+            }
+            return false;
+        }
+        // No column interdependencies need to be checked
+       return true;
     }
 
     @Override
--- src/main/java/uk/ac/ebi/pride/gui/data/mztab/model/ProteinData.java
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/model/ProteinData.java
@@ -13,7 +13,7 @@ import java.util.Set;
  * ---
  * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
  * All rights reserved.
- *
+ * <p>
  * This class manages Protein Section data in an mzTab document
  */
 
@@ -24,29 +24,41 @@ public class ProteinData implements MzTabSection {
     // deals with the mzTab data that is of interest for the submission process, that's why we don't need to hold any
     // more information other than the presence/absence of a column for quick and lightweight validation purposes
     public enum ColumnType {
-        ACCESSION,
-        DESCRIPTION,
-        TAXID,
-        SPECIES,
-        DATABASE,
-        DATABASE_VERSION,
-        SEARCH_ENGINE,
-        BEST_SEARCH_ENGINE_SCORE,
-        AMBIGUITY_MEMBERS,
-        MODIFICATIONS,
-        PROTEIN_COVERAGE,
-        PROTEIN_ABUNDANCE_STUDY_VARIABLE,
-        PROTEIN_ABUNDANCE_STDEV_STUDY_VARIABLE,
-        PROTEIN_ABUNDANCE_STD_ERROR_STUDY_VARIABLE,
-        SEARCH_ENGINE_SCORE_MS_RUN,
-        NUM_PSMS_MS_RUN,
-        NUM_PEPTIDES_DISTINCT_MS_RUN,
-        NUM_PEPTIDES_UNIQUE_MS_RUN,
-        PROTEIN_ABUNDANCE_ASSAY,
-        OPT_CUSTOM_ATTIBUTE,
-        GO_TERMS,
-        RELIABILITY,
-        URI
+        ACCESSION("accession"),
+        DESCRIPTION("description"),
+        TAXID("taxid"),
+        SPECIES("species"),
+        DATABASE("database"),
+        DATABASE_VERSION("database_version"),
+        SEARCH_ENGINE("search_engine"),
+        BEST_SEARCH_ENGINE_SCORE("best_search_engine_score[1-n]"),
+        AMBIGUITY_MEMBERS("ambiguity_members"),
+        MODIFICATIONS("modifications"),
+        PROTEIN_COVERAGE("protein_coverage"),
+        PROTEIN_ABUNDANCE_STUDY_VARIABLE("protein_abundance_study_variable[1-n]"),
+        PROTEIN_ABUNDANCE_STDEV_STUDY_VARIABLE("protein_abundance_stdev_study_variable[1-n]"),
+        PROTEIN_ABUNDANCE_STD_ERROR_STUDY_VARIABLE("protein_abundance_std_error_study_variable[1-n]"),
+        SEARCH_ENGINE_SCORE_MS_RUN("search_engine_score[1-n]_ms_run[1-n]"),
+        NUM_PSMS_MS_RUN("num_psms_ms_run[1-n]"),
+        NUM_PEPTIDES_DISTINCT_MS_RUN("num_peptides_distinct_ms_run[1-n]"),
+        NUM_PEPTIDES_UNIQUE_MS_RUN("num_peptides_unique_ms_run[1-n]"),
+        PROTEIN_ABUNDANCE_ASSAY("protein_abundance_assay[1-n]"),
+        OPT_CUSTOM_ATTIBUTE("opt_{identifier}"),
+        GO_TERMS("go_terms"),
+        RELIABILITY("reliability"),
+        URI("uri");
+
+        private String colTypeToString = "";
+
+        ColumnType(String colTypeToString) {
+            this.colTypeToString = colTypeToString;
+        }
+
+
+        @Override
+        public String toString() {
+            return colTypeToString;
+        }
     }
 
     // Present columns
@@ -65,6 +77,7 @@ public class ProteinData implements MzTabSection {
 
     /**
      * Returns true if a particular column type has been reported, at least once
+     *
      * @param columnType to look for
      * @return true if a particular column type has been reported, false otherwise
      */
@@ -80,7 +93,28 @@ public class ProteinData implements MzTabSection {
         return (getNumberOfColumns() != 0);
     }
 
+    /**
+     * Check that a set of column types are ALL present in this section
+     * @param columnTypes set of required column types
+     * @return true if they are all present, false otherwise
+     */
+    public boolean checkThatAllGivenColumnTypesArePresent(Set<ColumnType> columnTypes) {
+        return columnsFound.containsAll(columnTypes);
+    }
+
+    /**
+     * For a given set of column types, calculate which of them are missing in this section
+     * @param columnTypes set of column types
+     * @return a set of missing column types among the given ones in this section, empty set if they're all present
+     */
+    public Set<ColumnType> getMissingColumnTypesFromRequiredColumnTypes(Set<ColumnType> columnTypes) {
+        Set<ColumnType> result = new HashSet<>(columnTypes);
+        result.removeAll(columnsFound);
+        return result;
+    }
+
     @Override
+
     public boolean validate(MzTabDocument mzTabDocument, MzTabSectionValidator validator) throws InvalidMzTabSectionException {
         try {
             return validator.validate(mzTabDocument, this);
