6510dca00cf87fa3f2562c0bf5656c6ad778c09a
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-07-05 11:29:34 +0100

    validation algorithm draft for peptides and PSMs

--- src/main/java/uk/ac/ebi/pride/gui/data/mztab/model/DefaultMzTabSectionValidator.java
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/model/DefaultMzTabSectionValidator.java
@@ -187,14 +187,174 @@ public class DefaultMzTabSectionValidator extends MzTabSectionValidator {
 
     @Override
     public boolean validate(MzTabDocument mzTabDocument, PeptideData peptideData) throws MzTabSectionValidatorException {
-        // TODO
-        return false;
+        // Required columns for mzTab Identification type in Summary mode
+        Set<PeptideData.ColumnType> id_sum = new HashSet<>();
+        // Required columns for mzTab Identification type in Complete mode
+        Set<PeptideData.ColumnType> id_com = new HashSet<>();
+        // Required columns for mzTab Quantification type in Summary mode
+        Set<PeptideData.ColumnType> qu_sum = new HashSet<>();
+        // Required columns for mzTab Quantification type in Complete mode
+        Set<PeptideData.ColumnType> qu_com = new HashSet<>();
+        // Supported modes
+        Set<MetaData.MzTabMode> supportedModes = new HashSet<>(Arrays.asList(MetaData.MzTabMode.COMPLETE, MetaData.MzTabMode.SUMMARY));
+        // Supported types
+        Set<MetaData.MzTabType> supportedTypes = new HashSet<>(Arrays.asList(MetaData.MzTabType.IDENTIFICATION, MetaData.MzTabType.QUANTIFICATION));
+        // NO COLUMNS are used for identification type, but we don't enforcce columns to be missing
+        qu_sum.addAll(Arrays.asList(
+                PeptideData.ColumnType.SEQUENCE, PeptideData.ColumnType.ACCESSION, PeptideData.ColumnType.UNIQUE,
+                PeptideData.ColumnType.DATABASE, PeptideData.ColumnType.DATABASE_VERSION,
+                PeptideData.ColumnType.SEARCH_ENGINE, PeptideData.ColumnType.BEST_SEARCH_ENGINE_SCORE,
+                PeptideData.ColumnType.MODIFICATIONS, PeptideData.ColumnType.RETENTION_TIME,
+                PeptideData.ColumnType.RETENTION_TIME_WINDOW, PeptideData.ColumnType.CHARGE,
+                PeptideData.ColumnType.MASS_TO_CHARGE, PeptideData.ColumnType.PEPTIDE_ABUNDANCE_STUDY_VARIABLE,
+                PeptideData.ColumnType.PEPTIDE_ABUNDANCE_STDEV_STUDY_VARIABLE,
+                PeptideData.ColumnType.PEPTIDE_ABUNDANCE_STD_ERROR_STUDY_VARIABLE
+        ));
+        qu_com.addAll(Arrays.asList(
+                PeptideData.ColumnType.SEQUENCE, PeptideData.ColumnType.ACCESSION, PeptideData.ColumnType.UNIQUE,
+                PeptideData.ColumnType.DATABASE, PeptideData.ColumnType.DATABASE_VERSION,
+                PeptideData.ColumnType.SEARCH_ENGINE, PeptideData.ColumnType.BEST_SEARCH_ENGINE_SCORE,
+                PeptideData.ColumnType.MODIFICATIONS, PeptideData.ColumnType.RETENTION_TIME,
+                PeptideData.ColumnType.RETENTION_TIME_WINDOW, PeptideData.ColumnType.CHARGE,
+                PeptideData.ColumnType.MASS_TO_CHARGE, PeptideData.ColumnType.PEPTIDE_ABUNDANCE_STUDY_VARIABLE,
+                PeptideData.ColumnType.PEPTIDE_ABUNDANCE_STDEV_STUDY_VARIABLE,
+                PeptideData.ColumnType.PEPTIDE_ABUNDANCE_STD_ERROR_STUDY_VARIABLE,
+                PeptideData.ColumnType.SEARCH_ENGINE_SCORE_MS_RUN, PeptideData.ColumnType.PEPTIDE_ABUNDANCE_ASSAY,
+                PeptideData.ColumnType.SPECTRA_REF
+        ));
+        // Check for supported mode
+        if (!supportedModes.contains(mzTabDocument.getMetaData().getMode())) {
+            throw new MzTabSectionValidatorException("VALIDATION OF MZTAB MODE '" + mzTabDocument.getMetaData().getMode() + "' IS NOT SUPPORTED");
+        }
+        // Check for supported type
+        if (!supportedTypes.contains(mzTabDocument.getMetaData().getType())) {
+            throw new MzTabSectionValidatorException("VALIDATION OF MZTAB TYPE '" + mzTabDocument.getMetaData().getType() + "' IS NOT SUPPORTED");
+        }
+        // Select the testing set
+        Set<PeptideData.ColumnType> testingSet = null;
+        if (mzTabDocument.getMetaData().getType() == MetaData.MzTabType.IDENTIFICATION) {
+            if (mzTabDocument.getMetaData().getMode() == MetaData.MzTabMode.SUMMARY) {
+                // identification type - summary mode
+                testingSet = id_sum;
+            } else if (mzTabDocument.getMetaData().getMode() == MetaData.MzTabMode.COMPLETE) {
+                // identification type - complete mode
+                testingSet = id_com;
+            }
+        } else if (mzTabDocument.getMetaData().getType() == MetaData.MzTabType.QUANTIFICATION) {
+            // TODO Check "spectra_ref" presence if MS2 based quantification is used
+            if (mzTabDocument.getMetaData().getMode() == MetaData.MzTabMode.SUMMARY) {
+                // quantification type - summary mode
+                testingSet = qu_sum;
+            } else if (mzTabDocument.getMetaData().getMode() == MetaData.MzTabMode.COMPLETE) {
+                // quantification type - complete mode
+                testingSet = qu_com;
+            }
+        }
+        // Check the required columns
+        if (!peptideData.checkThatAllGivenColumnTypesArePresent(testingSet)) {
+            logger.error("Peptide section IS NOT VALID because of the following missing required columns:");
+            for (PeptideData.ColumnType missingColumnType :
+                    peptideData.getMissingColumnTypesFromRequiredColumnTypes(testingSet)) {
+                logger.error("MISSING Peptide Data column '" + missingColumnType.toString() + "' for mzTab "
+                        + mzTabDocument.getMetaData().getType().toString() + " Type, "
+                        + mzTabDocument.getMetaData().getMode().toString() + " Mode");
+            }
+            return false;
+        }
+        return true;
     }
 
     @Override
     public boolean validate(MzTabDocument mzTabDocument, PsmData psmData) throws MzTabSectionValidatorException {
-        // TODO
-        return false;
+        // Required columns for mzTab Identification type in Summary mode
+        Set<PsmData.ColumnType> id_sum = new HashSet<>();
+        // Required columns for mzTab Identification type in Complete mode
+        Set<PsmData.ColumnType> id_com = new HashSet<>();
+        // Required columns for mzTab Quantification type in Summary mode
+        Set<PsmData.ColumnType> qu_sum = new HashSet<>();
+        // Required columns for mzTab Quantification type in Complete mode
+        Set<PsmData.ColumnType> qu_com = new HashSet<>();
+        // Supported modes
+        Set<MetaData.MzTabMode> supportedModes = new HashSet<>(Arrays.asList(MetaData.MzTabMode.COMPLETE, MetaData.MzTabMode.SUMMARY));
+        // Supported types
+        Set<MetaData.MzTabType> supportedTypes = new HashSet<>(Arrays.asList(MetaData.MzTabType.IDENTIFICATION, MetaData.MzTabType.QUANTIFICATION));
+        // Fill in the required columns
+        id_sum.addAll(Arrays.asList(
+                PsmData.ColumnType.SEQUENCE, PsmData.ColumnType.PSM_ID, PsmData.ColumnType.ACCESSION,
+                PsmData.ColumnType.UNIQUE, PsmData.ColumnType.DATABASE, PsmData.ColumnType.DATABASE_VERSION,
+                PsmData.ColumnType.SEARCH_ENGINE, PsmData.ColumnType.SEARCH_ENGINE_SCORE,
+                PsmData.ColumnType.MODIFICATIONS, PsmData.ColumnType.SPECTRA_REF, PsmData.ColumnType.RETENTION_TIME,
+                PsmData.ColumnType.CHARGE, PsmData.ColumnType.EXP_MASS_TO_CHARGE,
+                PsmData.ColumnType.CALC_MASS_TO_CHARGE, PsmData.ColumnType.PRE, PsmData.ColumnType.POST,
+                PsmData.ColumnType.START, PsmData.ColumnType.END
+        ));
+        id_com.addAll(Arrays.asList(
+                PsmData.ColumnType.SEQUENCE, PsmData.ColumnType.PSM_ID, PsmData.ColumnType.ACCESSION,
+                PsmData.ColumnType.UNIQUE, PsmData.ColumnType.DATABASE, PsmData.ColumnType.DATABASE_VERSION,
+                PsmData.ColumnType.SEARCH_ENGINE, PsmData.ColumnType.SEARCH_ENGINE_SCORE,
+                PsmData.ColumnType.MODIFICATIONS, PsmData.ColumnType.SPECTRA_REF, PsmData.ColumnType.RETENTION_TIME,
+                PsmData.ColumnType.CHARGE, PsmData.ColumnType.EXP_MASS_TO_CHARGE,
+                PsmData.ColumnType.CALC_MASS_TO_CHARGE, PsmData.ColumnType.PRE, PsmData.ColumnType.POST,
+                PsmData.ColumnType.START, PsmData.ColumnType.END
+        ));
+        qu_sum.addAll(Arrays.asList(
+                PsmData.ColumnType.SEQUENCE, PsmData.ColumnType.PSM_ID, PsmData.ColumnType.ACCESSION,
+                PsmData.ColumnType.UNIQUE, PsmData.ColumnType.DATABASE, PsmData.ColumnType.DATABASE_VERSION,
+                PsmData.ColumnType.SEARCH_ENGINE, PsmData.ColumnType.SEARCH_ENGINE_SCORE,
+                PsmData.ColumnType.MODIFICATIONS, PsmData.ColumnType.SPECTRA_REF, PsmData.ColumnType.RETENTION_TIME,
+                PsmData.ColumnType.CHARGE, PsmData.ColumnType.EXP_MASS_TO_CHARGE,
+                PsmData.ColumnType.CALC_MASS_TO_CHARGE, PsmData.ColumnType.PRE, PsmData.ColumnType.POST,
+                PsmData.ColumnType.START, PsmData.ColumnType.END
+        ));
+        qu_com.addAll(Arrays.asList(
+                PsmData.ColumnType.SEQUENCE, PsmData.ColumnType.PSM_ID, PsmData.ColumnType.ACCESSION,
+                PsmData.ColumnType.UNIQUE, PsmData.ColumnType.DATABASE, PsmData.ColumnType.DATABASE_VERSION,
+                PsmData.ColumnType.SEARCH_ENGINE, PsmData.ColumnType.SEARCH_ENGINE_SCORE,
+                PsmData.ColumnType.MODIFICATIONS, PsmData.ColumnType.SPECTRA_REF, PsmData.ColumnType.RETENTION_TIME,
+                PsmData.ColumnType.CHARGE, PsmData.ColumnType.EXP_MASS_TO_CHARGE,
+                PsmData.ColumnType.CALC_MASS_TO_CHARGE, PsmData.ColumnType.PRE, PsmData.ColumnType.POST,
+                PsmData.ColumnType.START, PsmData.ColumnType.END
+        ));
+        // Check for supported mode
+        if (!supportedModes.contains(mzTabDocument.getMetaData().getMode())) {
+            throw new MzTabSectionValidatorException("VALIDATION OF MZTAB MODE '" + mzTabDocument.getMetaData().getMode() + "' IS NOT SUPPORTED");
+        }
+        // Check for supported type
+        if (!supportedTypes.contains(mzTabDocument.getMetaData().getType())) {
+            throw new MzTabSectionValidatorException("VALIDATION OF MZTAB TYPE '" + mzTabDocument.getMetaData().getType() + "' IS NOT SUPPORTED");
+        }
+        // Select the testing set
+        Set<PsmData.ColumnType> testingSet = null;
+        if (mzTabDocument.getMetaData().getType() == MetaData.MzTabType.IDENTIFICATION) {
+            if (mzTabDocument.getMetaData().getMode() == MetaData.MzTabMode.SUMMARY) {
+                // identification type - summary mode
+                testingSet = id_sum;
+            } else if (mzTabDocument.getMetaData().getMode() == MetaData.MzTabMode.COMPLETE) {
+                // identification type - complete mode
+                testingSet = id_com;
+            }
+        } else if (mzTabDocument.getMetaData().getType() == MetaData.MzTabType.QUANTIFICATION) {
+            // TODO Check "spectra_ref" presence if MS2 based quantification is used
+            if (mzTabDocument.getMetaData().getMode() == MetaData.MzTabMode.SUMMARY) {
+                // quantification type - summary mode
+                testingSet = qu_sum;
+            } else if (mzTabDocument.getMetaData().getMode() == MetaData.MzTabMode.COMPLETE) {
+                // quantification type - complete mode
+                testingSet = qu_com;
+            }
+        }
+        // Check the required columns
+        if (!psmData.checkThatAllGivenColumnTypesArePresent(testingSet)) {
+            logger.error("PSM section IS NOT VALID because of the following missing required columns:");
+            for (PsmData.ColumnType missingColumnType :
+                    psmData.getMissingColumnTypesFromRequiredColumnTypes(testingSet)) {
+                logger.error("MISSING PSM Data column '" + missingColumnType.toString() + "' for mzTab "
+                        + mzTabDocument.getMetaData().getType().toString() + " Type, "
+                        + mzTabDocument.getMetaData().getMode().toString() + " Mode");
+            }
+            return false;
+        }
+        return true;
     }
 
     @Override
