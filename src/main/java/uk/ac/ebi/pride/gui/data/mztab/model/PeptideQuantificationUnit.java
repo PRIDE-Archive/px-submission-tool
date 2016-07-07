43ce3bbf64db3265d2b13d8f31b2f646556cbf70
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-07-07 12:11:39 +0100

    peptide quantification unit model

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/model/PeptideQuantificationUnit.java
@@ -0,0 +1,21 @@
+package uk.ac.ebi.pride.gui.data.mztab.model;
+
+import uk.ac.ebi.pride.gui.data.mztab.exceptions.InvalidCvParameterException;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.model
+ * Timestamp: 2016-07-06 11:56
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+public class PeptideQuantificationUnit extends CvParameter {
+    public PeptideQuantificationUnit(String label, String accession, String name, String value) throws InvalidCvParameterException {
+        super(label, accession, name, value);
+    }
+
+    public PeptideQuantificationUnit(CvParameter cv) {
+        super(cv);
+    }
+}
