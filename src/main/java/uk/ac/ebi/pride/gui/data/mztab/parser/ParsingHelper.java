bffd7a4969aac8c156c288f3783d1876fb870ab3
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-07-07 14:42:44 +0100

    new parsing helper

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/ParsingHelper.java
@@ -0,0 +1,17 @@
+package uk.ac.ebi.pride.gui.data.mztab.parser;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.parser
+ * Timestamp: 2016-07-07 14:22
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ *
+ * This class contains a collection of parsing helpers that are useful for other parts of the mzTab parser
+ */
+public class ParsingHelper {
+    public static int getIndexInSquareBracketsFromIndexedKeyword(String keyword, String token) throws ParsingHelperException {
+
+    }
+}
