18997f8b448de3396ce88cb92097afbcb3e40861
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-07-11 15:19:03 +0100

    fixed modifications site parsing stack

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/MzTabFixedModSiteLineItemParsingHandler.java
@@ -0,0 +1,26 @@
+package uk.ac.ebi.pride.gui.data.mztab.parser;
+
+import uk.ac.ebi.pride.gui.data.mztab.parser.exceptions.LineItemParsingHandlerException;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.parser
+ * Timestamp: 2016-07-07 10:32
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+public abstract class MzTabFixedModSiteLineItemParsingHandler extends MzTabFixedModLineItemParsingHandler {
+    protected static final String MZTAB_FIXED_MOD_SITE_PROPERTY_KEY = "site";
+
+    @Override
+    protected boolean processEntry(MzTabParser context, long lineNumber, long offset) throws LineItemParsingHandlerException {
+        if (getPropertyKey().equals(MZTAB_FIXED_MOD_SITE_PROPERTY_KEY)) {
+            return doProcessEntry(context, lineNumber, offset);
+        }
+        return false;
+    }
+
+    // Delegate processing strategy
+    protected abstract boolean doProcessEntry(MzTabParser context, long lineNumber, long offset) throws LineItemParsingHandlerException;
+}
