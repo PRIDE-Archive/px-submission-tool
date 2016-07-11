cce8f64bbb0e742dfe8e510b1d180cba82a1d24b
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-07-11 17:31:05 +0100

    small molecule quantification unit parsing stack

---
+++ src/main/java/uk/ac/ebi/pride/gui/data/mztab/parser/MzTabSmallMoleculeQuantificationUnitLineItemParsingHandler.java
@@ -0,0 +1,69 @@
+package uk.ac.ebi.pride.gui.data.mztab.parser;
+
+import uk.ac.ebi.pride.gui.data.mztab.parser.exceptions.LineItemParsingHandlerException;
+import uk.ac.ebi.pride.gui.data.mztab.parser.exceptions.MetadataLineItemParserStrategyException;
+
+/**
+ * Project: px-submission-tool
+ * Package: uk.ac.ebi.pride.gui.data.mztab.parser
+ * Timestamp: 2016-07-07 11:53
+ * ---
+ * © 2016 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+public abstract class MzTabSmallMoleculeQuantificationUnitLineItemParsingHandler extends MetaDataLineItemParsingHandler implements MetaDataLineItemParsingHandler.LineItemBean {
+    protected static final String MZTAB_SMALL_MOLECULE_QUANTIFICATION_UNIT = "smallmolecule_quantification_unit";
+
+    // TODO - Refactor in the future to create external beans that handle this?
+// Bean defaults
+    protected static final String DEFAULT_LINE_ITEM_KEY = "";
+    protected static final String DEFAULT_PROPERTY_VALUE = "";
+    // Bean attributes
+    private String lineItemKey = DEFAULT_LINE_ITEM_KEY;
+    private String propertyValue = DEFAULT_PROPERTY_VALUE;
+
+    @Override
+    public String getLineItemKey() {
+        return lineItemKey;
+    }
+
+    @Override
+    public void setLineItemKey(String lineItemKey) {
+        this.lineItemKey = lineItemKey;
+    }
+
+    @Override
+    public String getPropertyValue() {
+        return propertyValue;
+    }
+
+    @Override
+    public void setPropertyValue(String propertyValue) {
+        this.propertyValue = propertyValue;
+    }
+
+    private void cleanBean() {
+        lineItemKey = DEFAULT_LINE_ITEM_KEY;
+        propertyValue = DEFAULT_PROPERTY_VALUE;
+    }
+
+    // TODO - Refactor out
+    @Override
+    protected boolean doParseLineItem(MzTabParser context, String line, long lineNumber, long offset) throws LineItemParsingHandlerException {
+        cleanBean();
+        try {
+            if (MetadataLineItemParserStrategy.parseLine(this, line)) {
+                if (getLineItemKey().equals(MZTAB_SMALL_MOLECULE_QUANTIFICATION_UNIT)) {
+                    // The line item key is ok, go ahead
+                    return processEntry(context, lineNumber, offset);
+                }
+            }
+        } catch (MetadataLineItemParserStrategyException e) {
+            throw new LineItemParsingHandlerException(e.getMessage());
+        }
+        return false;
+    }
+
+    // Delegate processing
+    protected abstract boolean processEntry(MzTabParser context, long lineNumber, long offset) throws LineItemParsingHandlerException;
+}
