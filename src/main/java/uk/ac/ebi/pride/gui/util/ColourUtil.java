cc30ced019297ecf02fadc32876f579c7d0c233a
Tobias Ternent <tobias@ebi.ac.uk>
2017-05-22 15:30:34 +0100

    Updated error background colour to red instead of yellow.

--- src/main/java/uk/ac/ebi/pride/gui/util/ColourUtil.java
+++ src/main/java/uk/ac/ebi/pride/gui/util/ColourUtil.java
@@ -15,7 +15,7 @@ public final class ColourUtil {
     public static final Color ALTER_ROW_COLOUR = new Color(229, 232, 236);
     public static final Color ROW_SELECTION_BACKGROUND = new Color(193, 210, 238);
     public static final Color ROW_SELECTION_FOREGROUND = Color.black;
-    public static final Color TEXT_FIELD_WARNING_COLOUR = new Color(255, 255, 153);
+    public static final Color TEXT_FIELD_WARNING_COLOUR = new Color(255, 190, 188);
     public static final Color TEXT_FIELD_NORMAL_COLOUR = Color.white;
     public static final Color HYPERLINK_COLOUR = new Color(98, 146, 179);
 
