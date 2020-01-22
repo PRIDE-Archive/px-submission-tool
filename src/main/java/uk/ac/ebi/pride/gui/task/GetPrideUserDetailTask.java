1416b72826a88aed0a992471e74109968270038e
selva <selva@ebi.ac.uk>
2020-01-22 10:54:14 +0000

    credential class and proxy

--- src/main/java/uk/ac/ebi/pride/gui/task/GetPrideUserDetailTask.java
+++ src/main/java/uk/ac/ebi/pride/gui/task/GetPrideUserDetailTask.java
@@ -1,16 +1,23 @@
 package uk.ac.ebi.pride.gui.task;
 
+import org.apache.http.HttpHost;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import uk.ac.ebi.pride.App;
 import uk.ac.ebi.pride.archive.submission.model.user.ContactDetail;
+import uk.ac.ebi.pride.gui.data.Credentials;
 import uk.ac.ebi.pride.toolsuite.gui.desktop.DesktopContext;
 import uk.ac.ebi.pride.toolsuite.gui.task.TaskAdapter;
+
+import java.util.Properties;
 
 /**
  * GetPrideUserDetailTask retrieves pride user details using pride web service
@@ -24,9 +31,7 @@ public class GetPrideUserDetailTask extends TaskAdapter<ContactDetail, String> {
 
     private final RestTemplate restTemplate;
 
-    public final String userName;
-
-    public final String password;
+    public final Credentials userCredentials;
 
     /**
      * Constructor
@@ -35,8 +40,7 @@ public class GetPrideUserDetailTask extends TaskAdapter<ContactDetail, String> {
      * @param password pride password
      */
     public GetPrideUserDetailTask(String userName, char[] password) {
-        this.userName = userName;
-        this.password = new String(password);
+        userCredentials = new Credentials(userName, new String(password));
         this.restTemplate = new RestTemplate();
     }
 
@@ -58,17 +62,28 @@ public class GetPrideUserDetailTask extends TaskAdapter<ContactDetail, String> {
         String userTokenUrl = context.getProperty("px.user.token.url");
         String userDetailUrl = context.getProperty("px.user.detail.url");
 
-
         try {
+            // set proxy
+            Properties props = System.getProperties();
+            String proxyHost = props.getProperty("http.proxyHost");
+            String proxyPort = props.getProperty("http.proxyPort");
+
+            if (proxyHost != null && proxyPort != null) {
+                logger.info("Using proxy server {} and port {}", proxyHost, proxyPort);
+                HttpComponentsClientHttpRequestFactory factory = ((HttpComponentsClientHttpRequestFactory) restTemplate.getRequestFactory());
+                DefaultHttpClient defaultHttpClient = (DefaultHttpClient) factory.getHttpClient();
+                HttpHost proxy = new HttpHost(proxyHost.trim(), Integer.parseInt(proxyPort));
+                defaultHttpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+            }
+
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
-            String requestBody = "{\"Credentials\":{\r\n  \"password\": \"" + password + "\",\r\n  \"username\": \"" + userName + "\"\r\n}}";
+            String requestBody = userCredentials.toString();
             HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
             final String token = restTemplate.exchange(userTokenUrl, HttpMethod.POST, entity, String.class).getBody();
 
             headers.set("Authorization", "Bearer " + token);
             entity = new HttpEntity<>(headers);
-            ContactDetail contactDetail = restTemplate.exchange(userDetailUrl, HttpMethod.GET, entity, ContactDetail.class).getBody();
             return restTemplate.exchange(userDetailUrl, HttpMethod.GET, entity, ContactDetail.class).getBody();
         } catch (Exception ex) {
             publish("Failed to login, please check user name or password");
